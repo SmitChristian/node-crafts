bf00d58da8358ba6b583bbf4b454b2f92a7a905d
smitChristian <crs29@email.sc.edu>
2024-03-24 20:39:25 -0400

    debugging

--- assignment14/public/script.js
+++ assignment14/public/script.js
@@ -2,7 +2,7 @@ async function showCrafts() {
     let response = await fetch("http://localhost:3000/api/crafts", {
         mode: "no-cors"
     });
-    let craftJSON = await response.json();
+    let craftJSON = await response.json()
     let mainContent = document.getElementById("content");
     let iteration = 1;
 
