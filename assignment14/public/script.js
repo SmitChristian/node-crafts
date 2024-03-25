b865d5655752c496b5be230441c62f5ccab6f704
smitChristian <crs29@email.sc.edu>
2024-03-24 20:30:03 -0400

    debugging

--- assignment14/public/script.js
+++ assignment14/public/script.js
@@ -1,5 +1,5 @@
 async function showCrafts() {
-    let response = await fetch("node-crafts-14.onrender.com/api/crafts");
+    let response = await fetch("http://localhost:3000/api/crafts");
     let craftJSON = await response.json();
     let mainContent = document.getElementById("content");
     let iteration = 1;
